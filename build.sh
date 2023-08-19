5582f5a1ab6f4a136a00949df2067fe5b8a712a0
wangchenglin.clark <wangcl.cw1@gmail.com>
2023-08-19 13:38:49 +0800

    add bash toolset

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ cd "$WORK_DIR" || exit 1
 
 cd extensions || exit
 
+# cpp toolset
 cd cpp_toolset || exit
-
 delta=$(git diff package.json)
 if [ -z "$delta" ]; then
     echo "... no changes and skipping build $(pwd)"
@@ -14,7 +14,17 @@ else
     vsce package
 fi
 
+# markdown toolset
 cd .. && cd markdown_toolset || exit
+delta=$(git diff package.json)
+if [ -z "$delta" ]; then
+    echo "... no changes and skipping build $(pwd)"
+else
+    vsce package
+fi
+
+# bash toolset
+cd .. && cd bash_toolset || exit
 delta=$(git diff package.json)
 if [ -z "$delta" ]; then
     echo "... no changes and skipping build $(pwd)"
